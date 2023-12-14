6d07bf6d4eb4e998222b2943cf56d3e94a2ca822
OxylabsOrg <103110131+OxylabsOrg@users.noreply.github.com>
2023-12-14 17:16:55 +0200

    Create opensea_curl.sh

---
+++ code examples/opensea_curl.sh
@@ -0,0 +1,4 @@
+curl --user USERNAME:PASSWORD \
+'https://realtime.oxylabs.io/v1/queries' \
+-H "Content-Type: application/json" \
+-d '{"source": "universal", "url": "https://opensea.io/collection/the-10k-collection-pass/drop"}'
